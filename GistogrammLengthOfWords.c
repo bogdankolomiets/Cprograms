641af7d0f1eb114c839aaa422aa32ac5ad5fae81
Bogdan <kolom996@gmail.com>
2016-02-11 16:55:57 +0200

    Horizontal Gistogram length of input words

---
+++ GistogrammLengthOfWords.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#define IN 1
+#define OUT 0
+
+/*Горизонтальная диаграмма длин вводимых слов*/
+
+main()
+{
+	int character, state;
+	
+	state = OUT;
+	
+	while ((character = getchar()) != EOF)
+	{
+		if (character == ' ' || character == '\n' || character == '\t')
+		{
+			state = OUT;
+			printf("\n");
+		} 
+		if (state == OUT)
+		{
+			state = IN;
+		}
+		if (character != ' ' && character != '\t' && character != '\n' && state == IN)
+		{
+			putchar('|');
+		}
+	}
+}
